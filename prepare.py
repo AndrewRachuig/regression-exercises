ca959d06cceb0fd037dcca3893c1e863edd6bdf3
Andrew Rachuig <andrew.rachuig@gmail.com>
2022-06-02 08:47:17 -0500

    updated functions

--- prepare.py
+++ prepare.py
@@ -16,18 +16,28 @@ def split_zillow(df):
     train, validate = train_test_split(train, train_size = 0.7, random_state = 1234)
     return train, validate, test
 
-def data_scaler(train, validate, test):
+def data_scaler(train, validate, test, columns_to_scale):
     '''
     This function takes in train, validate, test subsets of the cleaned zillow dataset and using the train subset creates a min_max 
     scaler. It thens scales the subsets and returns the train, validate, test subsets as scaled versions of the initial data.
 
-    Arguments: train, validate, test - split subsets from of the cleaned zillow dataframe
-    Return: scaled_train, scaled_validate, scaled_test - scaled versions of the initial unscaled dataframes
+    Arguments:  train, validate, test - split subsets from of the cleaned zillow dataframe
+                columns_to_scale - a list of column names to scale
+    Return: scaled_train, scaled_validate, scaled_test - dataframe with scaled versions of the initial unscaled dataframes 
     '''
+    train_scaled = train.copy()
+    validate_scaled = validate.copy()
+    test_scaled = test.copy()
     
     scaler = MinMaxScaler()
-    train_scaled = scaler.fit_transform(train)
-    validate_scaled = scaler.transform(validate)
-    test_scaled = scaler.transform(test)
+    
+    train_scaled[columns_to_scale] = pd.DataFrame(scaler.fit_transform(train[columns_to_scale]), 
+                                                  columns=train[columns_to_scale].columns.values).set_index([train.index.values])
+
+    validate_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(validate[columns_to_scale]),
+                                                  columns=validate[columns_to_scale].columns.values).set_index([validate.index.values])
+    
+    test_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(test[columns_to_scale]),
+                                                 columns=test[columns_to_scale].columns.values).set_index([test.index.values])
 
     return train_scaled, validate_scaled, test_scaled
